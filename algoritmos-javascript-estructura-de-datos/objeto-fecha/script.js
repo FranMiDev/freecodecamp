7d37db2c31577a10e7b7ce8036015dd7e9d23c0f
Francisco Miranda <164576996+FranMiDev@users.noreply.github.com>
2025-04-01 22:44:55 -0300

    comienzo de objeto fecha

---
+++ algoritmos-javascript-estructura-de-datos/objeto-fecha/script.js
@@ -0,0 +1,5 @@
+const currentDateParagraph = document.getElementById("current-date");
+const dateOptionsSelectElement = document.getElementById("date-options");
+
+console.log(currentDateParagraph);
+console.log(dateOptionsSelectElement);
