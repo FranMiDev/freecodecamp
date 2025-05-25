c646e685439bc607682d7a9b0506efae90d592cc
Francisco Miranda <164576996+FranMiDev@users.noreply.github.com>
2025-05-25 20:44:54 -0300

    final de la calculadora de estadisticas

--- algoritmos-javascript-estructura-de-datos/calculadora-estadisticas/script.js
+++ algoritmos-javascript-estructura-de-datos/calculadora-estadisticas/script.js
@@ -21,14 +21,42 @@ const getMedian = (array) => {
 
 const getMode = (array) => {
   const counts = {};
-  array.forEach((el) => {
-    if (counts[el]) {
-      counts[el] += 1;
-    } else {
-      counts[el] = 1;
-    }
-  });
-  return counts;
+  array.forEach((el) => (counts[el] = counts[el] ? counts[el] + 1 : 1));
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
+  }
+  const highest = Object.keys(counts).sort((a, b) => counts[b] - counts[a])[0];
+  const mode = Object.keys(counts).filter(
+    (el) => counts[el] === counts[highest]
+  );
+  return mode.join(", ");
+};
+
+const getRange = (array) => {
+  return Math.max(...array) - Math.min(...array);
+};
+
+const getVariance = (array) => {
+  const mean = getMean(array);
+  const variance =
+    array.reduce((acc, el) => {
+      const difference = el - mean;
+      const squared = difference ** 2;
+      return acc + squared;
+    }, 0) / array.length;
+  return variance;
+  // const differences = array.map((el) => el - mean);
+  // const squaredDifferences = differences.map((el) => el ** 2);
+  // const sumSquaredDifferences = squaredDifferences.reduce(
+  //   (acc, el) => acc + el,
+  //   0
+  // );
+};
+
+const getStandardDeviation = (array) => {
+  const variance = getVariance(array);
+  const standardDeviation = Math.sqrt(variance);
+  return standardDeviation;
 };
 
 // const numbersArr = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4];
@@ -49,8 +77,15 @@ const calculate = () => {
 
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  const mode = getMode(numbers);
+  const range = getRange(numbers);
+  const variance = getVariance(numbers);
+  const standardDeviation = getStandardDeviation(numbers);
 
-  console.log(getMode(numbers));
-  document.querySelector("#median").textContent = median;
   document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
+  document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
+  document.querySelector("#standardDeviation").textContent = standardDeviation;
 };
