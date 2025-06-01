87f058464c15539b7ed8745a3ddeeb19901982aa
Francisco Miranda <164576996+FranMiDev@users.noreply.github.com>
2025-06-01 12:55:39 -0300

    paso 54 - Hoja de Calculo

--- algoritmos-javascript-estructura-de-datos/hoja-de-calculo/script.js
+++ algoritmos-javascript-estructura-de-datos/hoja-de-calculo/script.js
@@ -8,7 +8,13 @@ const median = (nums) => {
   const middle = length / 2 - 1;
   return isEven(length)
     ? average([sorted[middle], sorted[middle + 1]])
-    : sorted(Math.ceil(middle));
+    : sorted[Math.ceil(middle)];
+};
+
+const spreadsheetFunctions = {
+  sum,
+  average,
+  median,
 };
 
 const range = (start, end) =>
@@ -20,20 +26,14 @@ const charRange = (start, end) =>
     String.fromCharCode(code)
   );
 
-const spreadsheetFunctions = {
-  sum,
-  average,
-  median,
-};
-
 const evalFormula = (x, cells) => {
   const idToText = (id) => cells.find((cell) => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
-  const rangeFromString = num1, num2 => range(parseInt(num1), parseInt(num2));
-  const elemValue = num => {
-    const inner = character =>{};
-    return inner;
-  };
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue = (num) => (character) => idToText(character + num);
+  const addCharacters = (character1) => (character2) => (num) =>
+    charRange(character1, character2).map(elemValue(num));
+  const rangeExpanded = x.replace(rangeRegex);
 };
 
 window.onload = () => {
@@ -53,7 +53,7 @@ window.onload = () => {
       input.type = "text";
       input.id = letter + number;
       input.ariaLabel = letter + number;
-      input.inchange = update;
+      input.onchange = update;
       container.appendChild(input);
     });
   });
