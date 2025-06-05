37940e3665f64e0ec6aee07370268e100a3eeae6
Francisco Miranda <164576996+FranMiDev@users.noreply.github.com>
2025-06-05 20:27:56 -0300

    paso 77 - Hoja de Calculo

--- algoritmos-javascript-estructura-de-datos/hoja-de-calculo/script.js
+++ algoritmos-javascript-estructura-de-datos/hoja-de-calculo/script.js
@@ -11,6 +11,7 @@ const infixEval = (str, regex) =>
   );
 const highPrecedence = (str) => {
   const regex = /([\d.]+)([*\/])([\d.]+)/;
+  const str2 = infixEval(str, regex);
 };
 
 const isEven = (num) => num % 2 === 0;
